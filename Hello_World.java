424670f8c0e86a68576ad0feadd39b5f367e839c
Koolau8081 <simonecanrock@gmail.com>
2024-08-14 10:31:42 -1000

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,17 @@
+/*
+ * This code is for the very first assignment. It does two simple prints and then I submit it.
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		//task 1: Print out "Hello World"
+		System.out.print("Hello World");
+		
+		//task 2: Print out  Hello World again
+		//and then a third time on next line
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+
+	}
+
+}
