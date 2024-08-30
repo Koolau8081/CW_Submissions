ea0633476c5d1157601e52b1663fc4388bd9247e
Koolau8081 <simonecanrock@gmail.com>
2024-08-30 13:24:28 -1000

    Create sphere.java

---
+++ sphere.java
@@ -0,0 +1,28 @@
+/* 
+ * This is my sphere class, if you put in the radius of 
+ * a sphere it will tell you its area and volume. 
+ */
+public class Intro_Sphere {
+
+	public static void main(String[] args) {
+		//Declare a radius as a double
+		double radius = 5.0;
+		//Declaring pi
+		double pie = 3.14;	
+		//Calculate its surface area
+		double sArea = 4.0*pie*(radius*radius);
+		//Calculate its volume
+		double volume = (4.0/3.0)*pie*(radius*radius*radius);
+		
+		//Print its values as ints
+	}
+
+}
+
+
+		
+		//print its values as doubles(in a sentence)
+		System.out.println("The side length of the sphere is " + (int)sl + ".");
+		System.out.println("It's surface area is " + (int)sa + ".");
+		System.out.print("It's volume is " + (int)volume + "." );
+		
