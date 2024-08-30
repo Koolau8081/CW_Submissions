4d57cf6acccf1cc2888c1099436dcf0d2821bde8
Koolau8081 <simonecanrock@gmail.com>
2024-08-30 13:11:42 -1000

    Create Homework1_PracticingSystemAndPrimitives.java

---
+++ Homework1_PracticingSystemAndPrimitives.java
@@ -0,0 +1,17 @@
+public class Homework1_PracticingSystemAndPrimitives {
+
+	public static void main(String[] args) {
+		// task 1: print out your name using the letter "O" in ASCII art.
+		System.out.println("OOO OOO O   O OOOO O   O OOOO");
+		System.out.println("O    O  OO OO O  O OO  O O   ");
+		System.out.println("OOO  O  O O O O  O O O O OOOO");
+		System.out.println("  O  O  O   O O  O O  OO O   ");
+		System.out.print("OOO OOO O   O OOOO O   O OOOO");
+		
+		// task 2: Write code to compute the following math problem: 
+	double topmult1 = 6.0*3.5;
+	double topmult2 = 1.5*5;
+	double topdiff = topmult1-topmult2;
+	double bottomdiff = 55.6-30.2;
+	double quo = topdiff/bottomdiff;
+	
