ac37dffd1a02da528f1f92ac79ec976283f09285
Koolau8081 <simonecanrock@gmail.com>
2024-09-24 10:58:48 -1000

    Create Shapes_main.java

---
+++ Shapes_main.java
@@ -0,0 +1,17 @@
+
+public class shapes_main {
+	
+	public static void main(String[] args) {
+
+	Circle circOne = new Circle(4.0);
+	
+	System.out.print(circOne.getRadius());
+	
+	System.out.print(Circle.getShape());
+	
+	
+	}
+	
+	//5 different shapes
+	//
+}
