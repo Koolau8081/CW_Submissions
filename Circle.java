6aabf5c8dbb09bf3f99bb7e88984064c6167b3ed
Koolau8081 <simonecanrock@gmail.com>
2024-09-24 10:59:39 -1000

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,31 @@
+
+public class Circle {
+
+	private double radius;
+	final private double pie =3.14;
+	
+	//constructor:
+	public Circle (double r) {
+	
+		radius = r;
+		
+	
+		
+	}
+	
+	
+	
+	//radius, diameter, circumference, area
+	
+	public static String getShape() {
+		
+		return "Circle";
+		
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	
+	
+}
