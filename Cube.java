794db7e2c5cef98d7fb9ba11e1e7c99a9112755e
Koolau8081 <simonecanrock@gmail.com>
2024-09-25 11:18:44 -1000

    Update Cube.java

--- Cube.java
+++ Cube.java
@@ -6,3 +6,23 @@
 //width
 //area
 //perimeter
+
+
+public class Cube {
+
+	//Attributes
+	private double side;
+	
+	//Constructor
+	public Cube(double s) {
+		
+		side = s;
+	}
+	
+	//Functions
+	public String getShape() {
+		String = 
+		
+	}
+	
+}
