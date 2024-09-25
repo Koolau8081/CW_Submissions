9d4b9dea5fe02b79ceec7ffdab2f5fc586cd5506
Koolau8081 <simonecanrock@gmail.com>
2024-09-25 10:12:19 -1000

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,24 @@
 
+public class Rectangle {
+
+	private double width;
+	private double length;
+	
+
+
+public Rectangle(int l, int w) {
+	length = l;
+	width = w;
+	
+	
+}
+
+public Rectangle(int s) {
+	length = s;
+	width = s;
+	
+}
+
+	
+
+}
