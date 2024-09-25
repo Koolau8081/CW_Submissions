5e5110589d09eda0245a23bf648c572ae5f586eb
Koolau8081 <simonecanrock@gmail.com>
2024-09-25 09:16:11 -1000

    Update Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -1,1 +1,49 @@
 
+public class Sphere {
+
+	//attributes
+	private double radius;
+	final private double pie = 3.14;
+	
+	//constructor
+	public Sphere (double r) {
+		
+		radius = r;
+		
+	}
+	
+	
+	//Functions
+	//Declare a radius as a double
+	public String getShape() {
+		
+		return("get shape");	
+	}
+	
+	
+	public double getRadius() {
+	
+		return radius;
+	}
+	
+	
+	public double getArea() {
+		
+		double area = 4.0*pie*(radius*radius);
+		return area;
+	}
+	
+	
+	public double getVolume() {
+		
+		double volume = (4.0/3.0)*pie*(radius*radius*radius);
+		return volume;
+	}
+	
+	
+
+			
+			
+}
+
+
