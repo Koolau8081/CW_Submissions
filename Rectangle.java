e0fdbb88b42959ba5e3046b31b859bed7ef39f86
Koolau8081 <simonecanrock@gmail.com>
2024-09-25 11:19:18 -1000

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,24 +1,49 @@
 
 public class Rectangle {
-
+	
+	
+	//attributes
 	private double width;
 	private double length;
 	
 
-
-public Rectangle(int l, int w) {
-	length = l;
-	width = w;
+	//constructor 
+	public Rectangle(int l, int w) {
+		length = l;
+		width = w;
 	
 	
-}
-
-public Rectangle(int s) {
-	length = s;
-	width = s;
+	}
+	//constructor 2
+	public Rectangle(int s) {
+		length = s;
+		width = s;
 	
-}
+	}
 
+	//functions
+	public String getshape() {
+		return "Rectangle";
+	}
+	
+	
+	public String getLengthAndWidth() {
+		return length + ", " + width;
+		
+	}
+
+	
+	public double getArea() {
+		double area = length*width;
+		return area;
+	}
+	
+	
+	public double perimeter() {
+		double perimeter = length + length + width + width;
+		return perimeter;
+	}
+	
 	
 
 }
