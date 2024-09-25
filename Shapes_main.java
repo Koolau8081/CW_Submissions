5e7569174e627046841723870025a2660a95498d
Koolau8081 <simonecanrock@gmail.com>
2024-09-25 10:11:22 -1000

    Update Shapes_main.java

--- Shapes_main.java
+++ Shapes_main.java
@@ -3,15 +3,25 @@ public class shapes_main {
 	
 	public static void main(String[] args) {
 
+	//Circle 1
 	Circle circOne = new Circle(4.0);
-	
 	System.out.print(circOne.getRadius());
+	System.out.println(Circle.getShape());
 	
-	System.out.print(Circle.getShape());
 	
+	//Rectangle
+	Rectangle rec1 = new Rectangle(3,7);
+	Rectangle rec2 = new Rectangle(4);
+	
+	//thing did in class shortcut goes at bottom of shape
+	System.out.print(circOne.toString());
 	
 	}
 	
+	
+	
 	//5 different shapes
 	//
+	
+	
 }
