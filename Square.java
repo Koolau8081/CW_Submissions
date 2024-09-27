f2c5ec13824e62a52700f2b2530b88456393070a
Koolau8081 <simonecanrock@gmail.com>
2024-09-27 03:02:45 -0700

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,5 +1,47 @@
 
 public class Square {
 
+	//Attributes
+	
+	private double side;
+	
+	
+	
+	//Constructor
+	
+	public Square(double s) {
+		
+		side = s;
+	
+	}
+	
+	//Functions
+	
+	public static String getShape() {
+		
+		return "Square";
+				
+	}
+	
+	public double getSide() {
+		
+		return side;
+		
+	}
+	
+	public double getArea() {
+		
+		double area = side*side;
+		return area;
+		
+	}
+	
+	public double getPerimeter() {
+		
+		double perimeter = side+side+side+side;
+		return perimeter;
+		
+	}
+	
+	
 }
-
