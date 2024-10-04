a259e797d9955319b0c0f1f885c5e8b920d3c371
Koolau8081 <simonecanrock@gmail.com>
2024-10-04 02:06:52 -0700

    Update Cube.java

--- project/shapes/Cube.java
+++ project/shapes/Cube.java
@@ -1,1 +1,58 @@
 
+public class Cube {
+
+	
+	
+//Attributes
+	private double side;
+	
+	
+//Constructor
+	public Cube(double s) {
+		
+		side = s;
+		
+	}
+	
+	
+//Functions
+	
+	/*
+	 * This method prints the shape name
+	 */
+	public static String getShape() {
+	
+		return ("Cube");
+		
+	}
+	
+	
+	/*
+	 * This method gets the side length
+	 */
+	public double getSide() {
+		return side;
+		
+	}
+	
+	/*
+	 * This method gets the volume
+	 */
+	public double getVolume() {
+		double volume = side*side*side;
+		return volume;
+	}
+	
+	/*
+	 * This method gets the Area
+	 */
+	public double getArea() {
+		double area = 6*(side*side);
+		return area;
+		
+	}
+	
+	
+	
+	
+}
