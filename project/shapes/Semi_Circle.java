256f4d93aec2dc6475200642c60ddd0287da5d8b
Koolau8081 <simonecanrock@gmail.com>
2024-10-04 02:07:37 -0700

    Update Semi_Circle.java

--- project/shapes/Semi_Circle.java
+++ project/shapes/Semi_Circle.java
@@ -1,1 +1,54 @@
 
+public class Semi_Circle {
+
+	//attributes
+	
+	private double radius;
+	final private double pie = 3.14;
+	
+	
+	
+	//constructor
+	public Semi_Circle(double r) {
+		
+		radius = r;
+		
+	}
+	
+	
+	//functions
+	
+	/*
+	 * This method prints the shape name
+	 */
+	public static String getShape() {
+		return "Semi_Circle";
+	}
+	
+	/*
+	 * This method gets the radius
+	 */
+	public double getRadius() {
+		return radius;
+	}
+	
+	/*
+	 * This method gets the Area
+	 */
+	public double getArea() {
+		
+		double area = (pie*(radius*radius))/2;
+		return area;
+	}
+	
+	
+	/*
+	 * This method gets the diameter
+	 */
+	public double getDiameter() {
+		double diameter = radius + radius;
+		return diameter;
+				
+	}
+	
+}
