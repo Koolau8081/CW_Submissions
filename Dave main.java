0a790c7f11d6548fc0282b6ffd6742b118e893db
Koolau8081 <simonecanrock@gmail.com>
2024-12-04 18:45:31 -1000

    Create Dave main.java

---
+++ Dave main.java
@@ -0,0 +1,47 @@
+
+
+import java.util.Scanner;
+
+
+/*
+ * This is the main class where everything comes together in the Dave parable.
+ */
+public class dave_Main {
+
+	static int uTextColor;
+	static int uDaveType;
+	
+	static Scanner myReader = new Scanner(System.in);
+	
+	
+/*
+ * This is my main method for my dave main. The main's main. 
+ */
+	public static void main(String[] args) {
+	
+		
+		Dave DaveDave = new Dave(daveMaker());
+		
+
+	}
+
+	
+/*
+ * This method 
+ */
+	public static int daveMaker() {
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
