89db247ba1296e9d1fad1bbd33d7fb614c4eceac
Koolau8081 <simonecanrock@gmail.com>
2024-12-04 18:45:55 -1000

    Create Dave.java

---
+++ Dave.java
@@ -0,0 +1,28 @@
+
+public class Dave {
+
+	//Attributes 
+	private double textColor;
+	private double daveType;
+	
+	
+	
+	//constructor
+	public Dave (int textColor, int daveType ) {
+		
+	
+		
+	}
+		
+	
+	
+	//Functions
+	public static String getName(String daveType)	{
+		
+		
+		return davetype;
+		
+	}
+	
+	public static 
+}
