b11b1212dcec46ce4ccf3d2e427794f5ce724268
Koolau8081 <simonecanrock@gmail.com>
2025-02-19 08:24:49 -1000

    Update Arrays_CodingBat_Homework_2.java

--- Codingbat/Arrays_CodingBat_Homework_2.java
+++ Codingbat/Arrays_CodingBat_Homework_2.java
@@ -7,3 +7,10 @@ public int[] rotateLeft3(int[] nums) {
   return nums;
 }
  
+public int[] reverse3(int[] nums) {
+  int revs = nums[0];
+  nums[0] = nums[2];
+  nums[2] = revs;
+  return nums;
+}
+
