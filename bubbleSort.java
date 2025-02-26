808be5cfbec73009252892219a5ebe85a0b3d195
Koolau8081 <simonecanrock@gmail.com>
2025-02-25 18:41:21 -0800

    Create bubbleSort.java

---
+++ bubbleSort.java
@@ -0,0 +1,31 @@
+
+public class bubbleSort_Assignment {
+
+	public static void main(String[] args) {
+		
+		int[] bubble = {3, 4, 7, 5, 2, 1, 9, 8, 10, 6};
+		
+
+	}
+
+		
+	public static int[] bubbleSort(int[] arr) {
+			
+			int holder = arr[0];
+			
+			for(int i = 1; i < arr.length; i++) {
+				
+				if(arr[i] < holder) {
+					
+					
+				}
+			
+				
+			}
+			
+			return arr;
+			
+	
+	}
+
+}
