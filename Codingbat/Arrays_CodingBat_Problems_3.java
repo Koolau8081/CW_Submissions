87d2d784869d1e6ba425ce83550c12a07c1632c4
Koolau8081 <simonecanrock@gmail.com>
2025-02-26 14:38:01 -1000

    Update Arrays_CodingBat_Problems_3.java

--- Codingbat/Arrays_CodingBat_Problems_3.java
+++ Codingbat/Arrays_CodingBat_Problems_3.java
@@ -125,3 +125,132 @@ public int[] midThree(int[] nums) {
   
   return arr;
 }
+
+public int maxTriple(int[] nums) {
+ 	int holder = nums[0];
+ 	
+	if(nums.length > 3){
+	  int midNum = (nums.length + 1) / 2;
+	  int[] arr = new int[3];
+	  arr[0] = nums[midNum-2];
+	  arr[1] = nums[midNum - 1];
+	  arr[2] = nums[midNum];
+	
+  	holder = arr[0];
+  	for(int n: arr) {				
+  		if(n > holder) {					
+  			holder = n;						
+  		}
+  	}  
+	}
+	else{	 
+  	holder = nums[0];
+  	  for(int n: nums) {					
+  			if(n > holder) { 						
+  				holder = n;					
+  			}
+  	}
+	}	
+return holder;
+}
+
+public int[] frontPiece(int[] nums) {
+  if(nums.length < 2){
+    int[] arr = nums;
+    return arr;
+  }
+  
+  else{
+    int[] arr = new int[2];
+    arr[0] = nums[0];
+    arr[1] = nums[1];
+    return arr;
+  }
+}
+
+public boolean unlucky1(int[] nums) {
+  boolean trueUnlucky = false;
+  boolean unlucky = false;
+  int unluckyIndex = 0;
+
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] == 1 && nums[i+1] == 3){
+      unlucky = true;
+      unluckyIndex = nums[i];
+     
+    }
+    if(unlucky == true && (unluckyIndex == nums[0] || unluckyIndex == nums[1] || unluckyIndex == nums[nums.length-2])){
+      trueUnlucky = true;
+    }
+    
+  }
+
+return trueUnlucky;
+
+}
+
+public int[] make2(int[] a, int[] b) {
+  int[] arr = new int[2];
+  
+  if(a.length == 1){
+    for(int num: a){
+      arr[0] = a[0];
+      arr[1] = b[0];
+    }
+  }
+  if(a.length > 1){
+    for(int num: a){
+      arr[0] = a[0];
+      arr[1] = a[1];
+    }
+  }
+  
+  if(a.length == 0){
+    arr[0] = b[0];
+    arr[1] = b[1];
+  }
+  
+  return arr;
+}
+public int[] front11(int[] a, int[] b) {
+ 
+  boolean emptyA = false;
+  boolean emptyB = false;
+  
+  if(a.length == 0){
+    emptyA = true;
+  }
+  
+  if(b.length == 0){
+    emptyB = true;
+  }
+  
+  if(emptyA == false && emptyB == false){
+    int[] arr = new int[2];
+    
+    arr[0] = a[0];
+    arr[1] = b[0];
+    
+    return arr;
+  }
+  
+  if((emptyA == true && emptyB == false) || (emptyA == false && emptyB == true)){
+      
+    int[] arr = new int[1]; 
+      
+    if(emptyA == true && emptyB == false){
+      arr[0] = b[0];
+    }
+    
+    if(emptyA == false && emptyB == true){
+      arr[0] = a[0];
+    }
+    return arr;
+  }
+  
+  if(emptyA == true && emptyB == true){
+    int[] arr = new int[0];
+  return arr;
+  }
+}
+
